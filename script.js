9ce03e342d1dd185b21b13e7bb8fb3f1aec1a5ed
ZayzX <140110873+ZayzX@users.noreply.github.com>
2025-03-27 22:48:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -15,13 +15,13 @@ const bonusCodes = {
 
 // Simplifie les grands nombres (1k, 1M, etc.)
 function simplifyNumber(num) {
-  if (num >= 1e12) return (num / 1e33).toFixed(2) + "Dc";
-  if (num >= 1e12) return (num / 1e30).toFixed(2) + "No";
-  if (num >= 1e12) return (num / 1e27).toFixed(2) + "Oc";
-  if (num >= 1e12) return (num / 1e24).toFixed(2) + "Sp";
-  if (num >= 1e12) return (num / 1e21).toFixed(2) + "Sx";
-  if (num >= 1e12) return (num / 1e18).toFixed(2) + "Qi";
-  if (num >= 1e12) return (num / 1e15).toFixed(2) + "Qa";
+  if (num >= 1e33) return (num / 1e33).toFixed(2) + "Dc";
+  if (num >= 1e30) return (num / 1e30).toFixed(2) + "No";
+  if (num >= 1e27) return (num / 1e27).toFixed(2) + "Oc";
+  if (num >= 1e24) return (num / 1e24).toFixed(2) + "Sp";
+  if (num >= 1e21) return (num / 1e21).toFixed(2) + "Sx";
+  if (num >= 1e18) return (num / 1e18).toFixed(2) + "Qi";
+  if (num >= 1e15) return (num / 1e15).toFixed(2) + "Qa";
   if (num >= 1e12) return (num / 1e12).toFixed(2) + "T";
   if (num >= 1e9) return (num / 1e9).toFixed(2) + "B";
   if (num >= 1e6) return (num / 1e6).toFixed(2) + "M";
